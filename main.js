53ef232dfe04836ccddbc824149ca27f45501f50
Adam Harris <aharris88@users.noreply.github.com>
2014-05-18 23:03:51 -0600

    Remove buttons

--- main.js
+++ main.js
@@ -4,19 +4,6 @@ $(function() {
   board.drawBackground();
   board.drawBoard();
 
-  d3.select('#upButton').on("click", function () {
-    board.move('up');
-  });
-  d3.select('#downButton').on("click", function () {
-    board.move('down');
-  });
-  d3.select('#leftButton').on("click", function () {
-    board.move('left');
-  });
-  d3.select('#rightButton').on("click", function () {
-    board.move('right');
-  });
-
   $('body').on("keyup", function (e) {
     switch(e.keyCode) {
       case 37:
