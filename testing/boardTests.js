04eb7f9aa80d2c053f4273b79a6234bcf7a53eed
Adam Harris <aharris88@users.noreply.github.com>
2014-05-18 20:02:38 -0600

    Add tests for getNewLocation

--- testing/boardTests.js
+++ testing/boardTests.js
@@ -70,6 +70,30 @@
     ]);
   });
 
+  test("shiftBoard() right then left", function() {
+    board._private.setBoard([
+      [2, 0, 3, 0],
+      [0, 1, 0, 0],
+      [2, 0, 1, 0],
+      [0, 0, 0, 1]
+    ]);
+    board._private.shiftBoard('right');
+    board._private.shiftBoard('left');
+    deepEqual(board._private.getBoard(), [
+      [2, 3, 0, 0],
+      [1, 0, 0, 0],
+      [2, 1, 0, 0],
+      [1, 0, 0, 0]
+    ]);
+  });
+
+  test('getNewLocation()', function() {
+    deepEqual(board._private.getNewLocation({x: 1, y: 1}, 'up'), {x: 1, y: 0}, 'up');
+    deepEqual(board._private.getNewLocation({x: 1, y: 1}, 'down'),{x: 1, y: 2}, 'down');
+    deepEqual(board._private.getNewLocation({x: 1, y: 1}, 'left'), {x: 0, y: 1}, 'left');
+    deepEqual(board._private.getNewLocation({x: 1, y: 1}, 'right'), {x: 2, y: 1}, 'right');
+  });
+
   test('getLocation()', function() {
     board.initialize({boardWidth: 100, padding: 5, tilesPerSide: 4});
 
